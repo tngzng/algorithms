57549e674205189a9c0bb790ed88f27dcb7ad579
stephtzhang <stephtzhang@gmail.com>
2015-11-14 15:39:16 -0500

    Return dict instead of tuple for readability

--- count_inversions.py
+++ count_inversions.py
@@ -6,20 +6,18 @@ def count_inversions(list, inversion_count = 0):
 
   :param list list: list containing comparable elements
   :param list list: list containing comparable elements
-  :returns: tuple w merged list and number of inversions
+  :returns: dict w list, a merged list, and count, the number of inversions
   """
   if len(list) < 2:
-    return (list, inversion_count)
+    return { "list": list, "count": inversion_count }
   mid_point = len(list) / 2
   # recursively count inversions in 1st half of input
   first_half = count_inversions(list[0:mid_point], inversion_count)
   # recursively count inversions in 2nd half of input
   second_half = count_inversions(list[mid_point:len(list)], inversion_count)
 
-  # TODO: indexing into the returned tuple is confusing
-  # consider returning a dict instead
-  running_inversion_count = first_half[1] + second_half[1]
-  return merge_and_count_inversions(first_half[0], second_half[0], running_inversion_count)
+  running_inversion_count = first_half["count"] + second_half["count"]
+  return merge_and_count_inversions(first_half["list"], second_half["list"], running_inversion_count)
 
 def merge_and_count_inversions(a, b, inversion_count):
   """
@@ -28,7 +26,7 @@ def merge_and_count_inversions(a, b, inversion_count):
 
   :param list a: ordered list
   :param list b: ordered list
-  :returns: tuple w merged list and number of inversions
+  :returns: dict w list, a merged list, and count, the number of inversions
   """
   i = 0
   j = 0
@@ -41,7 +39,7 @@ def merge_and_count_inversions(a, b, inversion_count):
       # concat merged w remainder of b if a's finished
       merged = merged + b[j:len(b)]
       j += 1
-      return (merged, inversion_count)
+      return { "list": merged, "count": inversion_count }
 
     try:
       b[j]
@@ -49,7 +47,7 @@ def merge_and_count_inversions(a, b, inversion_count):
       # concat merged w remainder of a if b's finished
       merged = merged + a[i:len(a)]
       i += 1
-      return (merged, inversion_count)
+      return { "list": merged, "count": inversion_count }
 
     if a[i] < b[j]:
       merged.append(a[i])
@@ -66,10 +64,10 @@ def merge_and_count_inversions(a, b, inversion_count):
       except IndexError:
         pass # a is exhausted
 
-  return (merged, inversion_count)
+  return { "list": merged, "count": inversion_count }
 
 list = [ 1, 2, 9, -1, 0]
-print count_inversions(list)[1]
+print count_inversions(list)["count"]
 
 # a = [1, 3, 5, 6]
 # b = [2, 4, 7, 8, 9]
