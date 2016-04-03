05b90dab50281e9b1cb35575d35db5f45d2ba15a
stephtzhang <stephtzhang@gmail.com>
2016-04-03 11:33:36 -0400

    Add pseudo for connected components

---
+++ connected_components.py
@@ -0,0 +1,8 @@
+def get_connected_components():
+    # assume nodes labeled 1 to n
+    # connected_components = []
+    # for i in 1..n
+        # if i not yet explored
+            # connected_component = bfs (graph, node i)
+            # connected_components.append(connected_component)
+    # return connected_components
