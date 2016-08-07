81a748df1ed040003f832cbe57c4ed91e71f872c
stephtzhang <stephtzhang@gmail.com>
2016-08-07 17:00:45 -0500

    Add heap extract min

--- heap.py
+++ heap.py
@@ -17,6 +17,24 @@ class Heap(object):
             position = parent_position
             self.bubble_up(key, position)
 
+    def extract_min(self):
+        res = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        position = 1
+        key = self.heap[position - 1]
+        self.bubble_down(key, position)
+        return res
+
+    def bubble_down(self, key, position):
+        min_child_pos = self.min_child_position(position)
+        if not min_child_pos:
+            return
+        min_child = self.heap[min_child_pos - 1]
+        if key > min_child:
+            self.swap(position, min_child_pos)
+            position = min_child_pos
+            self.bubble_down(key, position)
+
     def swap(self, position_1, position_2):
         i, j = (position_1 - 1, position_2 - 1)
         tmp = self.heap[i]
@@ -25,3 +43,16 @@ class Heap(object):
 
     def parent_position(self, position):
         return position / 2
+
+    def min_child_position(self, position):
+        (pos_1, pos_2) = (2 * position, 2 * position + 1)
+        try:
+            child_1 = self.heap[pos_1 - 1]
+        except IndexError:
+            return None
+        try:
+            child_2 = self.heap[pos_2 - 1]
+        except IndexError:
+            return pos_1
+        min_child_pos = pos_1 if child_1 < child_2 else pos_2
+        return min_child_pos
--- tests/test_heap.py
+++ tests/test_heap.py
@@ -16,6 +16,19 @@ class TestHeap(unittest.TestCase):
         heap.insert(1)
         self.assertFalse(self.heap_violation(heap.heap))
 
+    def test_extract_min(self):
+        heap = Heap()
+        heap.insert(4)
+        heap.insert(3)
+        heap.insert(2)
+        heap.insert(1)
+        min = heap.extract_min()
+        self.assertEqual(min, 1)
+        self.assertFalse(self.heap_violation(heap.heap))
+        min = heap.extract_min()
+        self.assertEqual(min, 2)
+        self.assertFalse(self.heap_violation(heap.heap))
+
     def heap_violation(self, heap):
         for i in range(len(heap)):
             position = i + 1
