e147cf7bccf1f2b1c50af44fe76c9e7b7a040cd1
stephtzhang <stephtzhang@gmail.com>
2017-07-15 16:45:28 -0400

    union_find: implement and test union

--- tests/test_union_find.py
+++ tests/test_union_find.py
@@ -13,8 +13,8 @@ class TestUnionFind(unittest.TestCase):
         d <- e
         """
         test_union_find = UnionFind()
-        # override internal data structure to test find
-        test_union_find.union_find = {
+        # override internal data structure to setup test
+        test_union_find._union_find = {
             'c': Node('c', parent_label='b', rank=0),
             'b': Node('b', parent_label='a', rank=1),
             'a': Node('a', parent_label='a', rank=2),
@@ -30,3 +30,54 @@ class TestUnionFind(unittest.TestCase):
         ]
         for child, expected_leader in expected_leaders:
             self.assertEqual(test_union_find.find(child), expected_leader)
+
+    def test_union__leaders_have_diff_ranks(self):
+        """
+        model a graph with two partitions:
+        a <- b <- c
+
+        d <- e
+        """
+        test_union_find = UnionFind()
+        # override internal data structure to setup test
+        test_union_find._union_find = {
+            'c': Node('c', parent_label='b', rank=0),
+            'b': Node('b', parent_label='a', rank=1),
+            'a': Node('a', parent_label='a', rank=2),
+            'e': Node('e', parent_label='d', rank=0),
+            'd': Node('d', parent_label='d', rank=1),
+        }
+        test_union_find.union('c', 'e')
+
+        smaller_ranked_leader = test_union_find._union_find['d']
+        larger_ranked_leader = test_union_find._union_find['a']
+
+        self.assertEqual(smaller_ranked_leader.parent_label, larger_ranked_leader.label)
+
+    def test_union__leaders_have_same_ranks(self):
+        """
+        model a graph with two partitions:
+        a <- b <- c
+
+        d <- e
+        """
+        test_union_find = UnionFind()
+        # override internal data structure to setup test
+        test_union_find._union_find = {
+            'b': Node('b', parent_label='a', rank=0),
+            'a': Node('a', parent_label='a', rank=1),
+            'e': Node('e', parent_label='d', rank=0),
+            'd': Node('d', parent_label='d', rank=1),
+        }
+        test_union_find.union('b', 'e')
+
+        leader_1 = test_union_find._union_find['d']
+        leader_2 = test_union_find._union_find['a']
+
+        # figure out which leader has become the new child and new parent
+        child = leader_1 if leader_1.parent_label != leader_1.label else leader_2
+        parent_leader = leader_1 if leader_1.parent_label == leader_1.label else leader_2
+
+        self.assertEqual(child.parent_label, parent_leader.label)
+        # the new parent's rank should have gone up by 1
+        self.assertEqual(parent_leader.rank, 2)
--- union_find.py
+++ union_find.py
@@ -1,22 +1,34 @@
 class UnionFind:
     def __init__(self, node_labels=[]):
-        self.union_find = {}
+        self._union_find = {}
         for node_label in node_labels:
-            self.union_find[node_label] = Node(node_label, parent_label=node_label)
+            self._union_find[node_label] = Node(node_label, parent_label=node_label)
 
     def find(self, node_label):
-        node = self.union_find[node_label]
-        parent = self.union_find[node.parent_label]
+        # TODO add lazy path compression
+        node = self._union_find[node_label]
+        parent = self._union_find[node.parent_label]
         while True:
             if parent.label == node.label:
                 # the partition leader's parent is itself
                 return parent.label
             else:
                 node = parent
-                parent = self.union_find[parent.parent_label]
+                parent = self._union_find[parent.parent_label]
 
-    def union(self, node_a, node_b):
-        pass
+    def union(self, node_label_a, node_label_b):
+        leader_label_a = self.find(node_label_a)
+        leader_label_b = self.find(node_label_b)
+        leader_a = self._union_find[leader_label_a]
+        leader_b = self._union_find[leader_label_b]
+        if leader_a.rank > leader_b.rank:
+            leader_b.set_parent_label(leader_label_a)
+        elif leader_a.rank == leader_b.rank:
+            leader_a.set_parent_label(leader_label_b)
+            # increment the leader's rank if it's merged with a subgraph of the same depth
+            leader_b.set_rank(leader_b.rank + 1)
+        else:
+            leader_a.set_parent_label(leader_label_b)
 
 
 class Node:
@@ -32,3 +44,13 @@ class Node:
     @property
     def label(self):
         return self._label
+
+    @property
+    def rank(self):
+        return self._rank
+
+    def set_parent_label(self, parent_label):
+        self._parent_label = parent_label
+
+    def set_rank(self, rank):
+        self._rank = rank
