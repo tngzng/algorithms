45a2362adafbbd3fa118ae8d57d641ed2517bfb4
tngzng <thetingzhang@gmail.com>
2017-07-29 23:20:21 -0400

    huffman_codes: make tests clearer

--- tests/test_huffman_codes.py
+++ tests/test_huffman_codes.py
@@ -17,29 +17,29 @@ class TestHuffmanCodes(unittest.TestCase):
 
     def test_compare_and_pop_smallest__second_q_smaller(self):
         q_1 = Queue()
-        q_1.enqueue((None, 1))
+        q_1.enqueue((None, 2))
         q_2 = Queue()
-        q_2.enqueue((None, 2))
-        output = _compare_and_pop_smallest(q_2, q_1)
+        q_2.enqueue((None, 1))
+        output = _compare_and_pop_smallest(q_1, q_2)
         self.assertEqual(output[1], 1)
 
     def test_compare_and_pop_smallest__first_q_empty(self):
         q_1 = Queue()
         q_2 = Queue()
         q_2.enqueue((None, 2))
-        output = _compare_and_pop_smallest(q_2, q_1)
+        output = _compare_and_pop_smallest(q_1, q_2)
         self.assertEqual(output[1], 2)
 
     def test_compare_and_pop_smallest__second_q_empty(self):
         q_1 = Queue()
         q_1.enqueue((None, 1))
         q_2 = Queue()
-        output = _compare_and_pop_smallest(q_2, q_1)
+        output = _compare_and_pop_smallest(q_1, q_2)
         self.assertEqual(output[1], 1)
 
     def test_traverse_children_and_assign_codes(self):
         """
-        test the following binary search tree
+        test the following binary tree
               a
              / \
             /   \
