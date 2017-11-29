4a08b6dee4a7897949a83d2995069c39cb4bc475
tngzng <thetingzhang@gmail.com>
2017-11-29 11:21:25 -0500

    huffman_codes: improve huffman_codes docstring

--- huffman_codes.py
+++ huffman_codes.py
@@ -4,8 +4,32 @@ def huffman_codes(char_frequencies):
     produce a representation in binary for each character that minimizes the memory
     needed to encode something in the alphabet
 
+    we do this by constructing a binary tree out of the chars in the alphabet,
+    where the lowest frequency chars have the largest depths from the root
+    and the highest frequency chars have the lowest depths from the root.
+
+    this allows us to output a binary representation of each char given its position in the tree
+    that is smaller for high frequency chars and larger for low frequency chars,
+    providing overall memory savings
+
     :param (list) char_frequencies: a list of tuples representing chars and their frequencies
     :returns (dict): a dict representing chars and their binary encodings
+
+    for ex, for the following char_frequencies:
+    [
+        ('c', 1),
+        ('a', 5),
+        ('t', 1),
+    ]
+
+    because 'a' occurs more than 'c' or 't', it should have the smallest encoding length
+
+    the final encodings output might look like:
+    {
+        'c': '00',
+        'a': '1',
+        't': '01',
+    }
     """
     # construct a queue of nodes, with highest priority given to the lowest frequency chars
     node_q = Queue()
@@ -55,16 +79,16 @@ def _traverse_children_and_assign_codes(parent, code_dict):
 
     binary representation is assigned by treating the left child as 0 and the right child as 1
 
-    :param HuffmanNode parent: the parent node whose children we will traverse
-    :param dict code_dict: a dictionary of running code assignments
+    :param (HuffmanNode) parent: the parent node whose children we will traverse
+    :param (dict) code_dict: a dictionary of running code assignments
 
     for ex, the following binary tree:
-         dec
+         cta
          / \
         /   \
-       de     c
+       ct     a
       / \
-     d   e
+     c   t
 
     would correspond to the following binary representations:
           .
@@ -76,9 +100,9 @@ def _traverse_children_and_assign_codes(parent, code_dict):
 
     and would produce this code assignment for its leaves:
     {
-        'd': '00',
-        'e': '01',
-        'c': '1',
+        'c': '00',
+        't': '01',
+        'a': '1',
     }
     """
     # when we hit a leaf, add it's binary code to our final output dict
