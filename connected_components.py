1ca72c299b9134de4c3a93e1830be3f2ed2290d3
ting <ting.zhang@buzzfeed.com>
2019-10-14 17:41:43 -0400

    add type hints

--- connected_components.py
+++ connected_components.py
@@ -1,4 +1,4 @@
-from typing import Mapping, List
+from typing import Mapping, List, Set
 from collections import deque
 
 
@@ -18,7 +18,7 @@ def get_connected_components(g: Mapping[int, List[int]]) -> List[List]:
             connected_components.append(connected_component)
     return connected_components
 
-def find_connected_component(g, node, explored):
+def find_connected_component(g: Mapping[int, List[int]], node: int, explored: Set[int]):
     """
     Breadth first search implementation that returns a connected component for a given starting node.
 
